7e7ebfd09695d857447a3d8feb1595a0e296858c
JanefrancessC <janefrancesschioma@gmail.com>
2026-03-28 02:53:13 +0000

    cat done

---
+++ implement-shell-tools/cat/customCat.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import { program } from "commander";
+import { promises as fs } from "node:fs";
+import process from "node:process";
+
+program
+  .name("customCat")
+  .description("CLI command to concatenate and print files")
+  .option("-n, --number", "Number all output lines starting, at 1")
+  .option("-b, --nonBlank", "Number only non-blank lines, starting at 1")
+  .argument("<files...>", "Files to read");
+
+program.parse();
+
+const argv = program.args;
+const options = program.opts();
+
+let lineNumber = 1;
+
+for (const filePath of argv) {
+  try {
+    const content = await fs.readFile(filePath, "utf-8");
+    const lines = content.split("\n");
+
+    if (lines[lines.length - 1] === "") lines.pop();
+
+    lines.forEach((line, index) => {
+      if (options.nonBlank) {
+        if (line.trim() !== "") {
+          process.stdout.write(`${(index + 1).toString().padStart(6)} ${line}\n`);
+          lineNumber++;
+        } else process.stdout.write(`${line}\n`);
+      } else if (options.number) {
+        process.stdout.write(`${(index + 1).toString().padStart(6)} ${line}\n`);
+      } else {
+        process.stdout.write(`${line}\n`);
+      }
+    });
+  } catch (error) {
+    console.error(`customCat: ${filePath}: No such file or directory.`);
+  }
+}
